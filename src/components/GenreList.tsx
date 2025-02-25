6f37f4965ef6a77bed5ae5c03abb13c9a5bc505a
miles <miles@local.com>
2025-02-25 16:06:43 +0000

    Show skeleton genres while loading

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,46 @@
-import useGenres from '../hooks/useGenres'
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const {data} = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+  if (error) return null;
 
   return (
-    <ul>
-      {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
-  )
-}
+    <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem key={skeleton} paddingY="5px">
+            <SkeletonCircle size="10" />
+          </ListItem>
+        ))}
 
-export default GenreList
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
