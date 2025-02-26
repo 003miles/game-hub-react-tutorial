d83d4188ee712cbb232c0989bc9a48696e96578c
miles <miles@local.com>
2025-02-26 15:26:02 +0000

    Implement searching

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,6 @@
 import noImage from "../assets/no-image-placeholder.webp";
 
 const getCroppedImageUrl = (url: string) => {
-  // TODO fix this function
   if (!url) return noImage;
 
   const target = "media/"
